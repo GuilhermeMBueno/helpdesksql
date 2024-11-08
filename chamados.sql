5ab4ce8db016eab9835afabd348b61f27f5e3946
Guilherme Bueno <gui.guimatheus@hotmail.com>
2024-11-08 00:07:45 -0300

    update

--- chamados.sql
+++ chamados.sql
@@ -7,6 +7,14 @@ SELECT
     TIME(T0.solvedate) AS 'Hora Solução',
     DATE(T0.closedate) AS 'Data Fechamento',
     TIME(T0.closedate) AS 'Hora Fechamento',
+    DATE(T0.begin_waiting_date) AS 'Dia da Atribuição',
+    TIME(T0.begin_waiting_date) AS 'Hora da Atribuição',
+    DATEDIFF(T0.begin_waiting_date, T0.date) AS 'Dias até Atribuir',
+CONCAT(
+    FLOOR(TIMESTAMPDIFF(MINUTE, T0.date, T0.begin_waiting_date) / 60), 'H, ',
+    MOD(TIMESTAMPDIFF(MINUTE, T0.date, T0.begin_waiting_date), 60), 'M'
+) AS 'Tempo até Atribuição',
+    T3.completename AS 'Categoria',
     CASE 
         WHEN T0.status = 1 THEN 'Novo'
         WHEN T0.status = 2 THEN 'Em andamento (Atribuído)'
@@ -17,9 +25,15 @@ SELECT
         ELSE 'Status Desconhecido'
     END AS 'Status',
     CONCAT(T1.firstname, ' ', T1.realname) AS 'Aberto Por',
-    CONCAT(T2.firstname, ' ', T2.realname) AS 'Ultima Atualizaçao Por'
+    CONCAT(T2.firstname, ' ', T2.realname) AS 'Ultima Atualizaçao Por',
+    DATEDIFF(T0.solvedate, T0.date) AS 'Dias até solução',
+    DATEDIFF(T0.closedate, T0.date) AS 'Dias até fechar',
+    CONCAT(
+    	FLOOR(TIMESTAMPDIFF(MINUTE, T0.date, T0.closedate) / 60), 'H, ',
+    	MOD(TIMESTAMPDIFF(MINUTE, T0.date, T0.closedate), 60), 'M'
+		  ) AS 'Hrs em aberto'
 FROM
    	glpi_tickets AS T0
 	INNER JOIN glpi_users AS T1 ON T0.users_id_recipient = T1.id
 	INNER JOIN glpi_users AS T2 ON T0.users_id_lastupdater = T2.id
-	
+	INNER JOIN glpi_itilcategories AS T3 ON T0.itilcategories_id = T3.id
